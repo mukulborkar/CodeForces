37a0fc10a84fabad888f69cc6b2cd1aa8c87bfc0
Mukul Borkar <mukulborkr03@gmail.com>
2021-05-21 16:00:03 +0530

    initial commit

---
+++ 112A-Petya and String.py
@@ -0,0 +1,10 @@
+def compare(s1,s2):
+  for i in range(len(s1)):
+    if s1.lower()[i] < s2.lower()[i]:
+      return -1
+    elif s1.lower()[i] > s2.lower()[i]:
+      return 1
+  return 0
+s1 = input()
+s2 = input()
+print(compare(s1,s2))
